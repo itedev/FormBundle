4483d4290eba48eb0c7489dd98d0035582fe7519
Dmitry <kislicynda@gmail.com>
2013-10-28 12:47:02 +0700

    add ite_debug function

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ Installation
 1. Add bundle to your project in composer.json:
 
 ```json
-"require": {
-    "ite/form-bundle": "dev-master",
+{
+    "require": {
+        "ite/form-bundle": "dev-master",
+    }
 }
 ```
 
@@ -276,3 +278,16 @@ public function replaceOptions($name, $options); // change options for existing 
 Field order
 -----------
 
+Debug
+-----
+If you want to dump huge objects in twig, and you cannot do it with default `dump()` function, you can use next way. Start listen for xDebug connections and add next construction to your twig template:
+
+```twig
+{% do ite_debug() %} {# for all variables in context #}
+```
+or
+
+```twig
+{% do ite_debug(form, entity) %} {# for specific variables #}
+```
+xDebug will automatically emits a breakpoint to the debug client on the specific line, and you can check your variable values in `$variables` var.
---
+++ Twig/Extension/DebugExtension.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ITE\FormBundle\Twig\Extension;
+
+use Twig_Environment;
+use Twig_Extension;
+use Twig_Template;
+
+/**
+ * Class DebugExtension
+ * @package ITE\FormBundle\Twig\Extension
+ */
+class DebugExtension extends Twig_Extension
+{
+    /**
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('ite_debug', array($this, 'debug'), array('needs_context' => true, 'needs_environment' => true)),
+        );
+    }
+
+    /**
+     * @param Twig_Environment $env
+     * @param $context
+     */
+    public function debug(Twig_Environment $env, $context)
+    {
+        if (!$env->isDebug()) {
+            return;
+        }
+
+        $variables = array();
+        $count = func_num_args();
+        if (2 === $count) {
+            foreach ($context as $key => $value) {
+                if (!$value instanceof Twig_Template) {
+                    $variables[$key] = $value;
+                }
+            }
+        } else {
+            for ($i = 2; $i < $count; $i++) {
+                $variables[] = func_get_arg($i);
+            }
+        }
+
+        unset($context, $count, $i, $key, $value);
+
+        if (function_exists('xdebug_break')) {
+            xdebug_break();
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ite_form.twig.debug_extension';
+    }
+
+}
