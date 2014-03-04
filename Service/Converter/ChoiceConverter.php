0354da7aa383806679b1aabc3be5413f840ad1ae
Dmitry <kislicynda@gmail.com>
2014-03-04 12:14:38 +0700

    update choice converter

--- Service/Converter/ChoiceConverter.php
+++ Service/Converter/ChoiceConverter.php
@@ -9,33 +9,33 @@ namespace ITE\FormBundle\Service\Converter;
 class ChoiceConverter
 {
     /**
-     * @param array $choices
-     * @return array
-     */
-    public function convertChoicesToOptions(array $choices)
-    {
-        $options = array();
-        foreach ($choices as $choice) {
-            $options[$choice['value']] = $choice['label'];
-        }
-
-        return $options;
-    }
-
-    /**
      * @param array $options
      * @return array
      */
     public function convertOptionsToChoices(array $options)
     {
         $choices = array();
-        foreach ($options as $value => $label) {
-            $choices[] = array(
+        foreach ($options as $option) {
+            $choices[$option['value']] = $option['label'];
+        }
+
+        return $choices;
+    }
+
+    /**
+     * @param array $choices
+     * @return array
+     */
+    public function convertChoicesToOptions(array $choices)
+    {
+        $options = array();
+        foreach ($choices as $value => $label) {
+            $options[] = array(
                 'value' => $value,
                 'label' => $label,
             );
         }
 
-        return $choices;
+        return $options;
     }
 } 
