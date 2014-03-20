9f2efe774d73b85b0993be9f86a1e064bc34b07e
Dmitry <kislicynda@gmail.com>
2014-03-20 20:15:10 +0700

    add twig extension for collection component

---
+++ Twig/Extension/Component/Collection/CollectionExtension.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace ITE\FormBundle\Twig\Extension\Component\Collection;
+
+use Symfony\Component\Form\FormView;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Twig_Environment;
+use Twig_Extension;
+use Twig_Template;
+
+/**
+ * Class CollectionExtension
+ * @package ITE\FormBundle\Twig\Extension\Component\Collection
+ */
+class CollectionExtension extends Twig_Extension
+{
+    /**
+     * @var PropertyAccessor $propertyAccessor
+     */
+    private $propertyAccessor;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->propertyAccessor = PropertyAccess::createPropertyAccessorBuilder()
+            ->disableExceptionOnInvalidIndex()
+            ->getPropertyAccessor();
+    }
+
+    /**
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('ite_is_collection', array($this, 'isCollection'), array('needs_context' => true)),
+            new \Twig_SimpleFunction('ite_is_collection_item', array($this, 'isCollectionItem'), array('needs_context' => true)),
+        );
+    }
+
+    /**
+     * @param $context
+     * @return bool
+     */
+    public function isCollection($context)
+    {
+        $blockPrefixes = $this->propertyAccessor->getValue($context, '[vars][block_prefixes]');
+
+        return in_array('collection', $blockPrefixes);
+    }
+
+    /**
+     * @param $context
+     * @param FormView $view
+     * @return bool
+     */
+    public function isCollectionItem($context, FormView $view = null)
+    {
+        $blockPrefixes = isset($view)
+            ? $this->propertyAccessor->getValue($view, 'parent.vars[block_prefixes]')
+            : $this->propertyAccessor->getValue($context, '[form].parent.vars[block_prefixes]');
+
+        return in_array('collection', $blockPrefixes);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ite_form.twig.collection_extension';
+    }
+
+}
