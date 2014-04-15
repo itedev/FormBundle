f1656c457356bfbff12d93da5943e2d52b8c521b
Dmitry <kislicynda@gmail.com>
2014-04-15 18:32:44 +0700

    fix select2 type

--- Form/Type/Plugin/Select2/AjaxEntityType.php
+++ Form/Type/Plugin/Select2/AjaxEntityType.php
@@ -134,7 +134,7 @@ class AjaxEntityType extends AbstractType
                     'url' => $options['url'],
                 ),
                 'multiple' => $options['multiple'],
-                'allowClear' => false !== $options['empty_value'],
+                'allowClear' => false !== $options['empty_value'] && null !== $options['empty_value'],
             ))
         );
     }
