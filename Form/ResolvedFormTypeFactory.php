ec35c14c3bd38c6f4b41402f68d79a3da0014743
c1tru55 <mr.c1tru55@gmail.com>
2017-01-23 19:38:03 +0600

    add support for ContainerAwareInterface in form types

--- Form/ResolvedFormTypeFactory.php
+++ Form/ResolvedFormTypeFactory.php
@@ -3,6 +3,8 @@
 namespace ITE\FormBundle\Form;
 
 use ITE\FormBundle\Proxy\ProxyFactory;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\ResolvedFormTypeFactory as BaseResolvedFormTypeFactory;
 use Symfony\Component\Form\ResolvedFormTypeInterface;
@@ -15,15 +17,22 @@ use Symfony\Component\Form\ResolvedFormTypeInterface;
 class ResolvedFormTypeFactory extends BaseResolvedFormTypeFactory
 {
     /**
+     * @var ContainerInterface $container
+     */
+    protected $container;
+
+    /**
      * @var ProxyFactory $proxyFactory
      */
     protected $proxyFactory;
 
     /**
+     * @param ContainerInterface $container
      * @param ProxyFactory $proxyFactory
      */
-    public function __construct(ProxyFactory $proxyFactory)
+    public function __construct(ContainerInterface $container, ProxyFactory $proxyFactory)
     {
+        $this->container = $container;
         $this->proxyFactory = $proxyFactory;
     }
 
@@ -35,6 +44,10 @@ class ResolvedFormTypeFactory extends BaseResolvedFormTypeFactory
         array $typeExtensions,
         ResolvedFormTypeInterface $parent = null
     ) {
+        if ($type instanceof ContainerAwareInterface) {
+            $type->setContainer($this->container);
+        }
+
         return new ResolvedFormType($this->proxyFactory, $type, $typeExtensions, $parent);
     }
 }
