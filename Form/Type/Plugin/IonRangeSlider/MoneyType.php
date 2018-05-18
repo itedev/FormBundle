8133caf6c457d45381517f483dc4111d7ea51817
c1tru55 <mr.c1tru55@gmail.com>
2018-05-18 21:28:09 +0600

    add new ion range slider form types

--- Form/Type/Plugin/IonRangeSlider/MoneyType.php
+++ Form/Type/Plugin/IonRangeSlider/MoneyType.php
@@ -2,38 +2,21 @@
 
 namespace ITE\FormBundle\Form\Type\Plugin\IonRangeSlider;
 
-use ITE\FormBundle\Form\DataTransformer\RangeToStringTransformer;
-use ITE\FormBundle\Form\Type\Plugin\Core\AbstractIntegerPluginType;
+use ITE\FormBundle\Form\Type\Plugin\Core\AbstractPluginType;
 use ITE\FormBundle\SF\Form\ClientFormTypeInterface;
 use ITE\FormBundle\SF\Form\ClientFormView;
 use ITE\FormBundle\SF\Plugin\IonRangeSliderPlugin;
-use Symfony\Component\Form\Extension\Core\DataTransformer\DataTransformerChain;
-use Symfony\Component\Form\FormBuilderInterface;
+use ITE\FormBundle\Util\MoneyUtils;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 
 /**
- * Class IntegerRangeType
+ * Class MoneyType
  *
  * @author c1tru55 <mr.c1tru55@gmail.com>
  */
-class IntegerRangeType extends AbstractIntegerPluginType implements ClientFormTypeInterface
+class MoneyType extends AbstractPluginType implements ClientFormTypeInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        parent::buildForm($builder, $options);
-
-        $partViewTransformer = new DataTransformerChain($builder->getViewTransformers());
-        $partModelTransformer = new DataTransformerChain($builder->getModelTransformers());
-        $builder->resetViewTransformers();
-        $builder->resetModelTransformers();
-
-        $builder->addViewTransformer(new RangeToStringTransformer($options['class'], ';', $partViewTransformer));
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -52,7 +35,15 @@ class IntegerRangeType extends AbstractIntegerPluginType implements ClientFormTy
      */
     public function buildClientView(ClientFormView $clientView, FormView $view, FormInterface $form, array $options)
     {
+        $parsedMoneyPattern = MoneyUtils::parseMoneyPattern($view->vars['money_pattern']);
+
         $predefinedOptions = [];
+        if ('prefix' === $parsedMoneyPattern['position']) {
+            $predefinedOptions['prefix'] = $parsedMoneyPattern['symbol'];
+        } elseif ('suffix' === $parsedMoneyPattern['position']) {
+            $predefinedOptions['postfix'] = $parsedMoneyPattern['symbol'];
+        }
+        $predefinedOptions['step'] = 1 / pow(10, $options['precision']);
 
         $clientView->setOption('plugins', [
             IonRangeSliderPlugin::getName() => [
@@ -62,7 +53,7 @@ class IntegerRangeType extends AbstractIntegerPluginType implements ClientFormTy
                     $predefinedOptions,
                     $options['plugin_options'],
                     [
-                        'type' => 'double',
+                        'type' => 'single',
                     ]
                 ),
             ],
@@ -74,7 +65,7 @@ class IntegerRangeType extends AbstractIntegerPluginType implements ClientFormTy
      */
     public function getParent()
     {
-        return 'ite_simple_range';
+        return 'money';
     }
 
     /**
@@ -82,6 +73,6 @@ class IntegerRangeType extends AbstractIntegerPluginType implements ClientFormTy
      */
     public function getName()
     {
-        return 'ite_ion_range_slider_integer_range';
+        return 'ite_ion_range_slider_money';
     }
 }
