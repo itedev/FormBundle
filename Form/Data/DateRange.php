50a905043acbb2a6c06e580f465b0c7c85173286
c1tru55 <mr.c1tru55@gmail.com>
2019-11-29 06:10:10 +1300

    Update DateRange class

--- Form/Data/DateRange.php
+++ Form/Data/DateRange.php
@@ -12,30 +12,30 @@ class DateRange extends Range
     /**
      * {@inheritdoc}
      */
-    public function setFrom($from)
+    public function getFrom($raw = false)
     {
-        if ($from instanceof \DateTime) {
-            $this->from = clone $from;
-            $this->from->setTime(0, 0, 0);
-        } else {
-            $this->from = null;
+        if (!$raw) {
+            $from = clone $this->from;
+            $from->setTime(0, 0, 0);
+
+            return $from;
         }
 
-        return $this;
+        return $this->from;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function setTo($to)
+    public function getTo($raw = false)
     {
-        if ($to instanceof \DateTime) {
-            $this->to = clone $to;
-            $this->to->setTime(23, 59, 59);
-        } else {
-            $this->to = null;
+        if (!$raw) {
+            $to = clone $this->to;
+            $to->setTime(0, 0, 0);
+
+            return $to;
         }
 
-        return $this;
+        return $this->to;
     }
 }
